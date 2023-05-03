752792631bad70f448e9ba67e1dbb2d42e9c6cda
Ashish Pal <ak987982@gmail.com>
2023-05-03 16:36:13 +0530

    Added UC1 Enter First Name

--- src/UserRegistrationMain.java
+++ src/UserRegistrationMain.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class UserRegistrationMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to User Registration Regex Problem");
+        System.out.print("Enter your first name: ");
+        String firstName = sc.nextLine();
+
+        if (firstName.matches("^[A-Z][a-zA-Z]{2,}$")) {
+            System.out.println("Valid first name: " + firstName);
+        } else {
+            System.out.println("Invalid first name. First name should start with a capital letter and have a minimum length of 3 characters.");
+        }
+        sc.close();
     }
 }
