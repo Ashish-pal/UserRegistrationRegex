294da2508d71a105f1f234dfeb18cb0b3f1502fc
Ashish Pal <ak987982@gmail.com>
2023-05-03 16:45:44 +0530

    Added UC2 Enter Last Name

--- src/UserRegistrationMain.java
+++ src/UserRegistrationMain.java
@@ -6,11 +6,20 @@ public class UserRegistrationMain {
         System.out.println("Welcome to User Registration Regex Problem");
         System.out.print("Enter your first name: ");
         String firstName = sc.nextLine();
+        System.out.print("Enter your last name: ");
+        String lastName = sc.nextLine();
+        boolean validFirstName = firstName.matches("^[A-Z][a-zA-Z]{2,}$");
+        boolean validLastName = lastName.matches("^[A-Z][a-zA-Z]{2,}$");
 
-        if (firstName.matches("^[A-Z][a-zA-Z]{2,}$")) {
-            System.out.println("Valid first name: " + firstName);
+        if(validFirstName && validLastName) {
+            System.out.println("Valid first and last names: " + firstName + " " + lastName);
         } else {
-            System.out.println("Invalid first name. First name should start with a capital letter and have a minimum length of 3 characters.");
+            if(!validFirstName) {
+                System.out.println("Invalid first name. First name should start with a capital letter and have a minimum length of 3 characters.");
+            }
+            if(!validLastName) {
+                System.out.println("Invalid last name. Last name should start with a capital letter and have a minimum length of 3 characters.");
+            }
         }
         sc.close();
     }
