667332524bd3ef18eca5eb3ea12fce7581f06bbd
Ashish Pal <ak987982@gmail.com>
2023-05-03 17:47:50 +0530

    Added UC5, UC6, UC7 and UC8. Rules for password matching.

--- src/UserRegistrationMain.java
+++ src/UserRegistrationMain.java
@@ -12,14 +12,17 @@ public class UserRegistrationMain {
         String email = sc.nextLine();
         System.out.print("Enter your mobile number: ");
         String mobile = sc.nextLine();
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine();
 
         boolean validFirstName = firstName.matches("^[A-Z][a-zA-Z]{2,}$");
         boolean validLastName = lastName.matches("^[A-Z][a-zA-Z]{2,}$");
         boolean validEmail = email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
         boolean validMobile = mobile.matches("^\\d{2} \\d{10}$");
+        boolean validPassword = password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8}$");
 
-        if(validFirstName && validLastName && validEmail && validMobile) {
-            System.out.println("Valid input entered: " + firstName + " " + lastName + ", " + email + ", " + mobile);
+        if(validFirstName && validLastName && validEmail && validMobile && validPassword) {
+            System.out.println("Valid input entered: " + firstName + " " + lastName + ", " + email + ", " + mobile + ", " + password);
         } else {
             if(!validFirstName) {
                 System.out.println("Invalid first name. First name should start with a capital letter and have a minimum length of 3 characters.");
@@ -32,6 +35,9 @@ public class UserRegistrationMain {
             }
             if(!validMobile) {
                 System.out.println("Invalid mobile number. Mobile number should follow the predefined format: country code followed by a space and 10 digit number.");
+            }
+            if(!validPassword) {
+                System.out.println("Invalid password. Password should have a minimum of 8 characters, at least 1 uppercase letter, at least 1 numeric digit, and exactly 1 special character (@$!%*?&).");
             }
         }
         sc.close();
